572259f61dd875cf5048b26cc784f9a87bc5ae46
Programming Hero <programming.hero1@gmail.com>
2022-05-08 07:29:42 -0500

    removed some warning for svg

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
-            <InfoCard cardTitle="Our Locations" bgClass="bg-neutral" img={marker}></InfoCard>
+            <InfoCard cardTitle="Our Locations" bgClass="bg-accent" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
