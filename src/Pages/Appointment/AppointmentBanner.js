75726ce3b0647d94c8732f4c5474af2b40837455
Programming Hero <programming.hero1@gmail.com>
2022-05-08 22:51:10 -0500

    load data from database

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
+import bg from '../../assets/images/bg.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
 const AppointmentBanner = ({date, setDate}) => {
     
     return (
-        <div class="hero min-h-screen">
+        <div style={{
+            background: `url(${bg})`,
+            backgroundSize: 'cover'
+        }} class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
                 <div>
--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,13 +8,13 @@ const AvailableAppointments = ({date}) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect( () =>{
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res=> res.json())
         .then(data => setServices(data));
     }, [])
 
     return (
-        <div>
+        <div className='my-10'>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
