403801c5e95ad083227d8c5fa0c452d351bfd34d
Programming Hero <programming.hero1@gmail.com>
2022-05-08 19:27:55 -0500

    added services card

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import React, {useState, useEffect} from 'react';
+import Service from './Service';
 
 const AvailableAppointments = ({date}) => {
     const [services, setServices] = useState([]);
@@ -13,8 +14,13 @@ const AvailableAppointments = ({date}) => {
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
-            <div>
-                
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service=><Service
+                    key={service._id}
+                    service={service}
+                    ></Service>)
+                }
             </div>
         </div>
     );
---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+    const { name, slots } = service;
+    return (
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title text-secondary">{name}</h2>
+                <p>{
+                    slots.length > 0
+                        ? <span>{slots[0]}</span>
+                        : <span className='text-red-500'>Try another date.</span>
+                }</p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <div class="card-actions justify-center">
+                    <button disabled={slots.length === 0} class="btn btn-secondary text-white uppercase">Book Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
