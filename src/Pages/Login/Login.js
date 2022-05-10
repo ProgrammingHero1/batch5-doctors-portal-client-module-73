3027c2d5d7324ce5db139240062490df57a88061
Programming Hero <programming.hero1@gmail.com>
2022-05-09 20:41:39 -0500

    handle login error and display loader

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
-    if (user) {
-        console.log(user);
+    let signInError;
+
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+    if(error || gError){
+        signInError= <p className='text-red-500'><small>{error?.message || gError?.message }</small></p>
+    }
+
+    if (gUser) {
+        console.log(gUser);
     }
 
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     }
 
     return (
@@ -71,7 +89,7 @@ const Login = () => {
                             </label>
                         </div>
 
-
+                        {signInError}
                         <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
                     <div className="divider">OR</div>
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div class="flex items-center justify-center ">
+            <div class="w-16 h-16 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loading;
